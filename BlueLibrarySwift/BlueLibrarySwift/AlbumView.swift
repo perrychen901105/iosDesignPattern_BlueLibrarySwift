4cdb890885b4e1d9aaa941a076ab2e54201178a7
Perry <chenzhipeng901105@gmail.com>
2015-03-17 17:57:47 +0800

    add new codes

---
+++ BlueLibrarySwift/BlueLibrarySwift/AlbumView.swift
@@ -0,0 +1,48 @@
+//
+//  AlbumView.swift
+//  BlueLibrarySwift
+//
+//  Created by chenzhipeng on 3/17/15.
+//  Copyright (c) 2015 Raywenderlich. All rights reserved.
+//
+
+import UIKit
+
+class AlbumView: UIView {
+
+    private let coverImage: UIImageView!
+    private let indicator: UIActivityIndicatorView!
+    
+    required init(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    init(frame: CGRect, albumCover: String) {
+        super.init(frame: frame)
+        backgroundColor = UIColor.blackColor()
+        coverImage = UIImageView(frame: CGRectMake(5, 5, frame.size.width - 10, frame.size.height - 10))
+        addSubview(coverImage)
+        indicator = UIActivityIndicatorView()
+        indicator.center = center
+        indicator.activityIndicatorViewStyle = .WhiteLarge
+        indicator.startAnimating()
+        addSubview(indicator)
+    }
+
+    func highlightAlbum(#didHighlightView: Bool) {
+        if didHighlightView == true {
+            backgroundColor = UIColor.whiteColor()
+        } else {
+            backgroundColor = UIColor.blackColor()
+        }
+    }
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
